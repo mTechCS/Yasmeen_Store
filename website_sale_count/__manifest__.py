7ba3a06b43f714264918fc46c25d2e30a990d144
mTechCS <43003578+mTechCS@users.noreply.github.com>
2020-04-12 17:42:32 +0300

    Add files via upload

---
+++ website_sale_count/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Website Sale Count',
+    'description': 'This module allows to display the Total Number of Products Sales Count in the Website.Sales Count on website, Website Sales count, Webshop sales couunt, Sale Count on Webshop, Show sales counter on shop page, purchase counter on shop page, Sales counter on website, Purchase counter on website, show number of sales on website, sales number on website, show number of sales on shop, sales number on shop',
+    'license':'OPL-1',
+    'category': 'eCommerce',
+    'version': '13.0.0.0',
+    'summary': 'Shows Sales/Puchases Counter on website/Shop Page',
+    'author': 'BrowseInfo',
+    'depends': ['website','website_sale'],
+    'data': [
+        'views/template.xml',
+    ],
+    'application': True,
+    'installable': True,
+    "images":['static/description/Banner.png'],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
